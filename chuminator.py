766a7941fcf432ccbd304844a41217d1ec28e02f
l0 <everGH1what>
2022-01-09 18:41:29 +0000

    initial commit

---
+++ chuminator.py
@@ -0,0 +1,12 @@
+# import binance as bnc
+import coinbase as cnb
+import jax.numpy as jnp
+import krakenex as krk
+# import PyQt5.Qt3DCore as qttd
+# import PyQt5.QtChart as qtch
+import PyQt5.QtCore as qtco
+# import PyQt5.QtDataVisualization as qtdv
+import PyQt5.QtGui as qtgu
+import qdarkstyle as qds
+import sqlalchemy as sa
+import zmq as zmq
